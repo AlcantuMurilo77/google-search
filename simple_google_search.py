58c484cd17036995c2550b17101630ec5f84faeb
Murilo Alcantu <157785297+AlcantuMurilo77@users.noreply.github.com>
2025-05-29 06:51:48 -0300

    Added a simple Google search automation

--- simple_google_search.py
+++ simple_google_search.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -7,6 +9,9 @@ service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("SQLAlchemy Documentation" + Keys.ENTER)
 
 time.sleep(10)
 
